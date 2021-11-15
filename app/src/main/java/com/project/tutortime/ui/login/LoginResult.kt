54567b6742c910b58467c175df2ab07093e138f9
AlmogJakov <almog1006@gmail.com>
2021-11-15 15:23:06 +0200

    added project files

--- app/src/main/java/com/project/tutortime/ui/login/LoginResult.kt
+++
@@ -1,9 +0,0 @@
-package com.project.tutortime.ui.login
-
-/**
- * Authentication result : success (user details) or error message.
- */
-data class LoginResult(
-    val success: LoggedInUserView? = null,
-    val error: Int? = null
-)
