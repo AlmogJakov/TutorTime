cdea0a75a4c49a11286fb88209ac6d3a4f0da2d2
AlmogJakov <almog1006@gmail.com>
2022-01-01 20:06:55 +0200

    fixed layout change after pressing Home Screen buttons.

--- app/src/main/java/com/project/tutortime/ui/home/HomeFragment.java
+++ app/src/main/java/com/project/tutortime/ui/home/HomeFragment.java
@@ -2,9 +2,12 @@ package com.project.tutortime.ui.home;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -67,6 +70,8 @@ public class HomeFragment extends Fragment {
         myProfileButton = binding.buttonProfile;
         searchButton = binding.buttonSearch;
         chatsButton = binding.buttonChats;
+        SpannableString ss = new SpannableString("(0)");
+        chatsButton.setText(ss);
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
